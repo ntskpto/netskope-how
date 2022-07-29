95a949d483e5c8b53ae3cd124810297674833a4a
PhilRumi <phil.rumi@t-online.de>
2022-07-29 15:46:33 +0200

    provider

--- data/provider.tf
+++ data/provider.tf
@@ -1,5 +1,8 @@
 provider "aws" {
-  region = "eu-central-1"
+  access_key = "${var.aws_access_key}" 
+  secret_key = "${var.aws_secret_key}"
+  region = "${var.aws_region}"
+  profile = "csw-profile"
 }
 
 terraform {
