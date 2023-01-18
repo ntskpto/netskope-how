7e98ff49633d92045ff6a2abbb90bb5bf3949554
marcus876 <mjaeger@netskope.com>
2023-01-18 15:16:43 +0100

    mj-change ubuntu publisher

--- data/vars.tf
+++ data/vars.tf
@@ -54,22 +54,30 @@ variable "student_instance_count" {
     type = number 
     description = "Number of studentPCs?"
 }
+variable "publisher_instance_ami" {
+    type = string
+    default = "ami-0756b97291156548b"
+}
 variable "publisher_instance_type" {
     type = string
     default = "t3.small"
 }
-variable "publisher_instance_ami" {
-    type = string
-    default = "ami-0dc0f92b55605df4d"
-}
-variable "ubuntu_publisher_instance_type" {
-    type = string
-    default = "t3.small"
-}
-variable "ubuntu_publisher_instance_ami" {
-    type = string
-    default = "ami-0019de00d22dbb1e4"
-}
+#variable "publisher_instance_ami" {
+#    type = string
+#    default = "ami-0dc0f92b55605df4d"
+#}
+#variable "publisher_instance_type" {
+#    type = string
+#    default = "t3.small"
+#}
+#variable "ubuntu_publisher_instance_ami" {
+#    type = string
+#    default = "ami-0019de00d22dbb1e4"
+#}
+#variable "ubuntu_publisher_instance_type" {
+#    type = string
+#    default = "t3.small"
+#}
 variable "webserver_instance_ami" {
     type = string
     default = "ami-071d25f19a56da4df"
@@ -78,21 +86,21 @@ variable "webserver_instance_type" {
     type = string
     default = "t3.small"
 }
-variable "guacamole_instance_type" {
-    type = string
-    default = "t3.large"
-}
 variable "guacamole_instance_ami" {
     type = string
     default = "ami-04d29be4d6f442172"
 }
-variable "master_instance_type" {
+variable "guacamole_instance_type" {
     type = string
     default = "t3.large"
 }
 variable "master_instance_ami" {
     type = string
     default = "ami-0c1e6dc9f1cc6950c"
+}
+variable "master_instance_type" {
+    type = string
+    default = "t3.large"
 }
 ############################################################
 #             route53.tf VARIABLES 
